7a24e14864559b8640887e93ec5b91f570102965
Alfonso Lizárraga <alfonso.lzrg@gmail.com>
2014-11-01 00:12:55 -0600

    español

--- hackmx/settings.py
+++ hackmx/settings.py
@@ -71,9 +71,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es_MX'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 
 USE_I18N = True
 
